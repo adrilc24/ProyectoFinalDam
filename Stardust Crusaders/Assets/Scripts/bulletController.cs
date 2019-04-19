846a7b32580a6fe77ae01767fae0800a195f576e
adrilc24 <31983069+adrilc24@users.noreply.github.com>
2019-04-19 19:56:22 +0200

    Arreglo disparo y cambio de vista
    Areglo bugs, y cambio de vista a vertical por motivos de comodidad al jugar.
    
    Cambio de Background.

---
+++ Stardust Crusaders/Assets/Scripts/bulletController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = new Vector3(0, speed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > 12)
+        {
+            Destroy(gameObject);
+            print("Disparo destruido");
+        }
+    }
+}
---
+++ Stardust Crusaders/Assets/Scripts/controllador.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public float speed = 12f;
+    public float padding = 1f;
+    public GameObject bulletFighter;
+
+    void Update()
+    {
+        Movimiento();
+
+        //Disparo
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Poner en marcha
+            InvokeRepeating("Fire", 0.001f, 0.25f);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //Parar el repetidor
+            CancelInvoke("Fire");
+        }
+
+    }
+
+    private void Movimiento()
+    {
+        //Mov. Vertical
+        float vInput = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, vInput * speed * Time.deltaTime, 0);
+
+
+        //Mov. Horizontal
+        float hInput = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(hInput * speed * Time.deltaTime, 0, 0);
+
+        //Clamping
+        float newX = Mathf.Clamp(transform.position.x, -7 + padding, 7 - padding);
+        float newY = Mathf.Clamp(transform.position.y, -12 + padding, 12 - padding);
+
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+
+
+    void Fire()
+    {
+        var fighter = GameObject.Find("fighter");
+        if (fighter != null)
+        {
+            Vector3 newRightPosition = fighter.transform.position + Vector3.up * 0.3f + Vector3.up * 0.5f;
+            //Vector3 newLeftPosition = fighter.transform.position + Vector3.left * 0.3f + Vector3.left * 0.5f;
+            Instantiate(bulletFighter, newRightPosition, Quaternion.identity);
+            //Instantiate(bulletFighter, newLeftPosition, Quaternion.identity);
+        }
+    }
+
+}
