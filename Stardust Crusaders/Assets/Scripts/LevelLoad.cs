41a1dfdb64630cf5714abd14250e712b11851533
adrilc24 <31983069+adrilc24@users.noreply.github.com>
2019-05-05 20:26:12 +0200

    Añadido Menus

---
+++ Stardust Crusaders/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelLoad : MonoBehaviour
+{
+   public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
